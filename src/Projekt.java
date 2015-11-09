7f15616b85fd43e216a093e6d3614ab06c0fa8af
kleemets <kristoleemets@hotmail.com>
2015-11-09 21:50:33 +0200

    JsonProov

--- src/Projekt.java
+++ src/Projekt.java
@@ -2,9 +2,21 @@
  * Created by Leemets on 1.11.2015.
  */
 import java.io.*;
-import org.json.JSONObject;
 
-public class Projekt {
+import jdk.nashorn.internal.parser.JSONParser;
+import org.json.JSONObject;
+import org.json.*;
+
+public class Projekt{
+
     public static void main(String[] args) {
+        JSONParser pars= new JSONParser();
+
+        try {
+            Object obj = pars.parse(new FileReader("C:\\Users\\Leemets\\Desktop\\dpd.json"));
+       
+        }catch (Exception e){
+            e.printStackTrace();
+        }
     }
 }
